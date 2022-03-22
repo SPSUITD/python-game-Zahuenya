e38d63651d66c4954e082b8935f0aca894e7affc
Zahuenya <Chomiichi@gmail.com>
2022-03-22 23:38:11 +0300

    добавила view игры

--- src/game.py
+++ src/game.py
@@ -1,8 +1,22 @@
 import arcade
 
 
+class GameView(arcade.View):
+    """
+    Главный класс игры
+    """
+
+    def __init__(self):
+        """
+        Функция инициализатор для объекта игры
+        """
+        super().__init__()
+
+
 def main():
     window = arcade.Window(1024, 800, "Suzy")
+    game_view = GameView()
+    window.show_view(game_view)
     arcade.run()
 
 
