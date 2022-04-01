b4d7b31955ba16becc60d465ac55f29ddea92fd3
Zahuenya <Chomiichi@gmail.com>
2022-04-02 00:12:17 +0300

    тест загрузки карты и тайлов

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 import arcade
+import os
 
 
 class GameView(arcade.View):
@@ -11,6 +12,29 @@ class GameView(arcade.View):
         Функция инициализатор для объекта игры
         """
         super().__init__()
+
+        self.root_dir = os.path.dirname(os.path.abspath(__file__))
+
+        self.tile_map = None
+        self.scene = None
+
+    def setup(self):
+        """Init game world"""
+
+        map_name = f"{self.root_dir}/map.tmx"
+
+        self.tile_map = arcade.load_tilemap(map_name, 0.5)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+
+    def on_show_view(self):
+        self.setup()
+
+
+    def on_draw(self):
+        """Render game frame"""
+        self.clear()
+        self.scene.draw()
 
 
 def main():
---
+++ src/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="test" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="girl.png" width="199" height="306"/>
+</tileset>
